03c826cfc80cb9aa7584ab1ca1a486de7f606fc1
Lukas Jungmann <lukas.jungmann@oracle.com>
2013-10-10 10:32:35 +0000

    remove workaround for MS1252 bug (fixed in JDK5+)
    svn path=/trunk/; revision=333

--- codemodel/codemodel/src/main/java/com/sun/codemodel/util/EncoderFactory.java
+++ codemodel/codemodel/src/main/java/com/sun/codemodel/util/EncoderFactory.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright (c) 1997-2010 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1997-2013 Oracle and/or its affiliates. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
@@ -43,46 +43,20 @@
  */
 package com.sun.codemodel.util;
 
-import java.lang.reflect.Constructor;
 import java.nio.charset.Charset;
 import java.nio.charset.CharsetEncoder;
 
 /**
  * Creates {@link CharsetEncoder} from a charset name.
  * 
- * Fixes a MS1252 handling bug in JDK1.4.2.
- * 
  * @author
  *     Kohsuke Kawaguchi (kohsuke.kawaguchi@sun.com)
  */
 public class EncoderFactory {
-    
-	public static CharsetEncoder createEncoder( String encodin ) {
+
+    public static CharsetEncoder createEncoder( String encodin ) {
         Charset cs = Charset.forName(System.getProperty("file.encoding"));
         CharsetEncoder encoder = cs.newEncoder();
-        
-        if( cs.getClass().getName().equals("sun.nio.cs.MS1252") ) {
-            try {
-                // at least JDK1.4.2_01 has a bug in MS1252 encoder.
-                // specifically, it returns true for any character.
-                // return a correct encoder to workaround this problem
-                
-                // statically binding to MS1252Encoder will cause a Link error
-                // (at least in IBM JDK1.4.1)
-            	@SuppressWarnings("unchecked")
-                Class<? extends CharsetEncoder> ms1252encoder = (Class<? extends CharsetEncoder>) Class.forName("com.sun.codemodel.util.MS1252Encoder");
-                Constructor<? extends CharsetEncoder> c = ms1252encoder.getConstructor(new Class[]{
-                    Charset.class
-                });
-                return c.newInstance(new Object[]{cs});
-            } catch( Throwable t ) {
-                // if something funny happens, ignore it and fall back to
-                // a broken MS1252 encoder. It's probably still better
-                // than choking here.
-                return encoder;
-            }
-        }
-        
         return encoder;
     }
 }
